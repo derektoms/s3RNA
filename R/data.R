c563576e37f70cf1b6dcabdebfdab73aa5bf9ee7
Derek Toms <derek.toms@ucalgary.ca>
2019-11-27 12:46:35 -0700

    Update and rename miRNA.R to R/data.R

---
+++ R/data.R
@@ -0,0 +1,20 @@
+NULL
+#' miRNA
+#'
+#' Full microRNA sequencing data of the pig (Sus scrofa) ovarian follicle 
+#' used in Pan, Toms, et al. (2020).
+#'
+#' @format miRNA contains unprocessed miRNA read counts (\code{miRNA$raw.count}) 
+#' and column data (\code{miRNA$col.dat}) that can be used for analysis.
+#'
+"miRNA"
+
+#' snoRNA
+#'
+#' Full small nucleolar RNA sequencing data of the pig (Sus scrofa) ovarian follicle 
+#' used in Pan, Toms, et al. (2020).
+#'
+#' @format snoRNA contains unprocessed snoRNA read counts (\code{snoRNA$raw.count}) 
+#' and column data (\code{snoRNA$col.dat}) that can be used for analysis.
+#'
+"snoRNA"
