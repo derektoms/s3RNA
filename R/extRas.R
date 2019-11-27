85c961d1eb976c269bd553c24eed5ee59b6e54d6
Derek Toms <derek.toms@ucalgary.ca>
2019-11-27 11:52:19 -0700

    Update extRas.R

--- R/extRas.R
+++ R/extRas.R
@@ -1,4 +1,3 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("Welcome to receptoR!\nTo launch the shiny application, use 'launchApp()'")
-  shiny::addResourcePath('www', system.file("shiny/www", package = "receptoR"))
+  packageStartupMessage("Welcome to the s3RNA package!\nThis package provides the data from Pan, Toms et al. (2020).")
 }
